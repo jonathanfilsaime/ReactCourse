bb8cbbaa021454fabd0ea7655e362ae8a97905f3
jonathanfilsaime <jonathanfilsaime@gmail.com>
2018-08-11 14:38:05 -0500

    start the new app burger builder

--- react/my-app/src/Components/Persons/Person/Person.js
+++ react/my-app/src/Components/Persons/Person/Person.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css'
 
-const person = (props) => {
-    return (
-        <div className={classes.Person} >
-            <p onClick={props.click}>hi there I'm {props.name} and I'm {props.age} years old</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    );
-};
+class Person extends Component{
+    render(){
+        return (
+            <div className={classes.Person} >
+                <p onClick={this.props.click}>hi there I'm {this.props.name} and I'm {this.props.age} years old</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        );
+    };
+}
 
-export default person;
+export default Person;
